431c5265f1a19975c42ae8ec4b68adfb51531f81
Jason Holm <holmjason2@gmail.com>
2019-05-23 17:22:45 -0700

    added setup function to ui

--- TermProject/src/UserInterface.java
+++ TermProject/src/UserInterface.java
@@ -2,16 +2,17 @@ import javax.swing.*;
 
 public class UserInterface
 {
-    public void addButton()
+    public void setup()
     {
-        JFrame f=new JFrame();//creating instance of JFrame
+        JFrame f = new JFrame();//creating instance of JFrame
 
-        JButton b=new JButton("click");//creating instance of JButton
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        JLabel title = new JLabel("Welcome to the Java Chat Application!", SwingConstants.CENTER);
+        JButton reg = new JButton("Register New User");//creating instance of JButton
+        reg.setBounds(130,100,100, 40);//x axis, y axis, width, height
 
-        f.add(b);//adding button in JFrame
+        f.add(reg);//adding button in JFrame
 
-        f.setSize(400,500);//400 width and 500 height
+        f.setSize(600,800);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
     }
